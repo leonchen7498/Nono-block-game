5839f5473ccb86e8edb059e90de98cfb1a279f0b
Leon Chen <leonchen@live.nl>
2019-09-06 12:47:29 +0200

    tilemap added, colored blocks added
    also changed mobile orientation from landscape to portrait

---
+++ Assets/Scripts/blocks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class blocks : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Sprite sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Use Vector3Int to place a tile via the script
+        /*Tile tile = new Tile();
+        tile.sprite = sprite;
+        tilemap.SetTile(new Vector3Int(1,0,0), tile);
+        tilemap.RefreshAllTiles();*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
