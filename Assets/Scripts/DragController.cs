4abb00c0e6dd73d35a577bafcf5e0d93311af2c4
JordiBontje <jordibontje11@gmail.com>
2019-09-11 11:09:14 +0200

    match 3 overhaul

--- Assets/Scripts/DragController.cs
+++ Assets/Scripts/DragController.cs
@@ -8,10 +8,11 @@ namespace Assets.Scripts
     {
         [HideInInspector]
         public static bool isDragging;
+        public static string carryingBlock;
         // Start is called before the first frame update
         void Start()
         {
-
+            carryingBlock = "red";
         }
 
         // Update is called once per frame
--- Assets/Scripts/PlaceBlock.cs
+++ Assets/Scripts/PlaceBlock.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 namespace Assets.Scripts { 
     public class PlaceBlock : MonoBehaviour
     {
-        public GameObject tempObject;
+        public GameObject BlueObject;
+        public GameObject RedObject;
+        public GameObject YellowObject;
+        public bool visible;
+
+
         // Start is called before the first frame update
         void Start()
         {
-        
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            visible = false;
         }
 
         // Update is called once per frame
@@ -22,10 +28,35 @@ namespace Assets.Scripts {
                 RaycastHit2D hit = Physics2D.Raycast(touchPositionToWorld, Vector2.zero);
 
                 //if circle is hit and it is the correct circle
-                if (hit.collider != null && hit.collider.gameObject == this.gameObject)
+                if (hit.collider != null && hit.collider.gameObject == this.gameObject && visible == false)
                 {
-                    Instantiate(tempObject, transform.position, Quaternion.identity);
-                    transform.position = new Vector3(transform.position.x, transform.position.y + 125, transform.position.z);
+                    gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                    visible = true;
+                    
+                } else if (hit.collider != null && hit.collider.gameObject == this.gameObject && visible == true)
+                {
+                    switch (DragController.carryingBlock)
+                    {
+                        case "yellow":
+                            Instantiate(YellowObject, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+                            DragController.carryingBlock = "blue";
+                            break;
+                        case "blue":
+                            Instantiate(BlueObject, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+                            DragController.carryingBlock = "red";
+                            break;
+                        case "red":
+                            Instantiate(RedObject, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+                            DragController.carryingBlock = "yellow";
+                            break;
+                    }
+
+                    gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                    visible = false;
+                } else
+                {
+                    gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                    visible = false;
                 }
             }
         }
