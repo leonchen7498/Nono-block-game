25ffce379c6d5d79ba22151fe4555e0f734cba40
Leon Chen <leonchen@live.nl>
2019-10-11 13:42:34 +0200

    last fixes before last release 😭
    -blocks didnt stick for some reason but well it's fixed now

--- Assets/Scripts/MatchThree.cs
+++ Assets/Scripts/MatchThree.cs
@@ -23,6 +23,22 @@ namespace Assets.Scripts
             if (!isStatic)
             {
                 movePlaceholderToBlock();
+                checkIfBlockIsSameAsThisBlock(this.transform.position.x - 120);
+                checkIfBlockIsSameAsThisBlock(this.transform.position.x + 120);
+            }
+        }
+
+        void checkIfBlockIsSameAsThisBlock(float positionX)
+        {
+            RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector2(positionX, transform.position.y), Vector2.zero);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider.tag == this.gameObject.tag)
+                {
+                    OnTriggerEnter2D(hit.collider);
+                    return;
+                }
             }
         }
 
@@ -71,7 +87,9 @@ namespace Assets.Scripts
         void OnTriggerEnter2D(Collider2D collider)
         {
             // Debug-draw all contact points and normals
-            if (this.gameObject.GetComponent<BoxCollider2D>().attachedRigidbody == collider.attachedRigidbody || this.gameObject.GetComponent<PolygonCollider2D>().attachedRigidbody == collider.attachedRigidbody || this.tag != collider.gameObject.tag)
+            if (this.gameObject.GetComponent<BoxCollider2D>().attachedRigidbody == collider.attachedRigidbody || 
+                this.gameObject.GetComponent<PolygonCollider2D>().attachedRigidbody == collider.attachedRigidbody || 
+                this.tag != collider.gameObject.tag)
             {
             }
             else
@@ -84,50 +102,55 @@ namespace Assets.Scripts
                     }
                 }
 
-                adjacentMatchingBlocks.Add(collider.gameObject);
+                if (collider.gameObject != null &&
+                    collider.gameObject.transform.position.x == gameObject.transform.position.x)
+                {
+                    adjacentMatchingBlocks.Add(collider.gameObject);
+                    if (collider.gameObject.name.Contains("Glass") && adjacentMatchingBlocks.Count == 1)
+                    {
+                        adjacentMatchingBlocks.ForEach(deleteBlock => Destroy(deleteBlock));
+                        SoundController.glassBroken = true;
+                        return;
+                    }
+                    else
+                    {
+                        float blockPositionY = Mathf.Round(collider.gameObject.transform.position.y / 120.0f) * 120;
+                        collider.gameObject.transform.position = new Vector2(collider.gameObject.transform.position.x, blockPositionY);
+                        collider.gameObject.GetComponent<MatchThree>().toStatic();
+                    }
+                }
+                else if (collider.gameObject != null && 
+                    Mathf.Abs(collider.gameObject.transform.position.y - gameObject.transform.position.y) <= 60)
+                {
+                    adjacentMatchingBlocks.Add(collider.gameObject);
+                    float yPosition = Mathf.Round(gameObject.transform.position.y / 120.0f) * 120;
+                    collider.gameObject.transform.position = new Vector2(collider.gameObject.transform.position.x, yPosition);
+                    collider.gameObject.GetComponent<MatchThree>().toStatic();
+                }
 
                 if (adjacentMatchingBlocks.Count >= 2)
                 {
                     foreach (GameObject block in adjacentMatchingBlocks)
                     {
+                        if (block.name.Contains("Glass") && adjacentMatchingBlocks.Count == 1)
+                        {
+                            SoundController.glassBroken = true;
+                        }
                         Destroy(block);
                     }
                     Destroy(gameObject);
                 }
-
-                foreach (GameObject block in adjacentMatchingBlocks)
-                {
-                    if (block != null)
-                    {
-                        if (block.transform.position.x == gameObject.transform.position.x)
-                        {
-                            if (block.name.Contains("Glass") && adjacentMatchingBlocks.Count == 1)
-                            {
-                                adjacentMatchingBlocks.ForEach(deleteBlock => Destroy(deleteBlock));
-                                Debug.Log("he");
-                                return;
-                            }
-                            else
-                            {
-                                float blockPositionY = Mathf.Round(block.transform.position.y / 120.0f) * 120;
-                                block.transform.position = new Vector2(block.transform.position.x, blockPositionY);
-                                block.GetComponent<MatchThree>().toStatic();
-                            }
-                        }
-                        else if (Mathf.Abs(block.transform.position.y - gameObject.transform.position.y) <= 60)
-                        {
-                            float yPosition = Mathf.Round(gameObject.transform.position.y / 120.0f) * 120;
-                            block.transform.position = new Vector2(block.transform.position.x, yPosition);
-                            block.GetComponent<MatchThree>().toStatic();
-                        }
-                    }
-                }
             }
         }
-
+        
         void OnDestroy()
         {
             foreach (GameObject deletable in tempBlocks)
+            {
+                Destroy(deletable);
+            }
+
+            foreach (GameObject deletable in adjacentMatchingBlocks)
             {
                 Destroy(deletable);
             }
